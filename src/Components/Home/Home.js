7a11e9b316bbd6be60e4ec60f8a9a3f3668b4b7f
Anubhav Johri <anubhavj22@gmail.com>
2021-02-21 21:46:56 +0530

    Added more debouncing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import {useState, useEffect, useRef, useCallback} from 'react';
 import collegeList from "../../Utils/colleges.json";
 import CollegeCard from "../Common/CollegeCard";
+import "./Home.css";
 
 function Home (){
     const [ collegeData, setCollegeData ] = useState([]);
@@ -8,6 +9,7 @@ function Home (){
     const [ currSize, setCurrSize ] = useState(10);
     const [ isEnd , setIsEnd ] = useState(false);
     const parentDivRef = useRef(null);
+    let modifiedGetMoreData = null;
 
     /*DEBOUNCED FUNCTION:- CREATED DEBOUNCED FUNCTION TO FIRE SCROLL EVENT ONLY AFTER A CERTAIN
     TIME TO REDUCE THE NUMBER OF TIMES IT'S FIRED*/
@@ -21,6 +23,7 @@ function Home (){
 
     useEffect(()=>{
         document.body.style.overflow = 'hidden';
+        modifiedGetMoreData = debouncedHandleScroll(getMoreData, 2000);
         const modifiedHandleScroll = debouncedHandleScroll(handleScroll, 600);
         parentDivRef.current.addEventListener("scroll", modifiedHandleScroll)
         setCollegeData(collegeList.colleges.slice(0,currSize));
@@ -29,28 +32,30 @@ function Home (){
         return ()=>parentDivRef&&parentDivRef.current&&parentDivRef.current.removeEventListener("scroll", modifiedHandleScroll);
     },[])
 
+    function getMoreData(){
+        let temp = 0;
+        setCurrSize(n=>temp=n);
+        if((temp+10)<=50)
+        {
+            setCurrSize(temp+10);
+            setCollegeData([...collegeList.colleges.slice(0,temp+10)]);
+            setIsLoading(false);
+            setIsEnd(false);
+        }
+        else{
+            setIsLoading(false);
+            setIsEnd(true);
+        }
+        parentDivRef.current.scrollTop = parentDivRef.current.scrollTop-400;
+    }
+
     /** HANDLES SCROLL AND CHECKS IF WE HAVE TO LOAD MORE DATA BASED ON THE FACT
      * WHETHER WE HAVE REACHED ALMOST AT THE END OF THE SCROLL
      */
     function handleScroll(e){
-        if(((e.target.scrollTop + e.target.clientHeight) >= (e.target.scrollHeight))&&!isLoading){
+        if(((e.target.scrollTop + e.target.clientHeight+200) >= (e.target.scrollHeight))&&!isLoading){
             setIsLoading(true);
-            setTimeout(()=>{
-                let temp = 0;
-                setCurrSize(n=>temp=n);
-                if((temp+10)<=50)
-                {
-                    setCurrSize(temp+10);
-                    setCollegeData([...collegeList.colleges.slice(0,temp+10)]);
-                    setIsLoading(false);
-                    setIsEnd(false);
-                }
-                else{
-                    setIsLoading(false);
-                    setIsEnd(true);
-                }
-                parentDivRef.current.scrollTop = parentDivRef.current.scrollTop-300; 
-            },2000);
+            modifiedGetMoreData();
         }
     }
 
@@ -58,6 +63,7 @@ function Home (){
         <div 
          ref={parentDivRef} 
          style={{ height: '656px', overflowY:'scroll'}}>
+             <span className="ParentDivTitle"><b>Colleges in North India</b></span>
             {collegeData.map(college=>
             <CollegeCard 
             key={college.college_name}
@@ -78,7 +84,7 @@ function Home (){
             offerText = {college.offertext}
             />)}
             {isLoading?<span style={{fontSize:'60px', marginLeft:'522px', paddingBottom:'180px', display:'block'}}>Loading.....</span>:null}
-            {isEnd?<span style={{fontSize:'60px', marginLeft:'522px', paddingBottom:'180px'}}>No More Data To Fetch</span>:null}
+            {(isEnd&&!isLoading)?<span style={{fontSize:'60px', marginLeft:'522px', paddingBottom:'300px'}}>No More Data To Fetch</span>:null}
         </div>
     )
 }
